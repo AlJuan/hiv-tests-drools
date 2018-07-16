671df358ee36c109a7890cf0eb664dd9ee5f3adb
AlJuan <juan6691@gmail.com>
2018-07-16 12:36:19 -0300

    archtype

---
+++ rules-hiv/src/main/java/fiuba/fallas/App.java
@@ -0,0 +1,13 @@
+package fiuba.fallas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
